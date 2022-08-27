844066541c1d8dc7ccc168ecaaac0b06a73f35e6
fish <fisseha.137@gmail.com>
2022-08-27 22:55:59 +0300

    performance enhancements

--- pages/1_user_overview.py
+++ pages/1_user_overview.py
@@ -20,6 +20,7 @@ from dataHandler import *
 # set the page and title
 st.set_page_config(page_title="User overview", layout="wide")
 
+
 @st.cache
 def loadData():
     """
@@ -38,21 +39,25 @@ def loadDataFromDB():
     #df = pd.csv('name', engine)
     return df
 
+
 @st.cache
 def loadDataFromCSV():
     df = pd.read_csv('data/sample_overview_data.csv.bz2')
     return df
-    
 
-def displayData():
+
+baseDF = loadDataFromCSV()
+
+
+def displayData(baseDF):
     st.text('Overall Data')
     # TODO
     # df = loadDataFromDB()
-    df = loadDataFromCSV()
-    st.write(df)
+    # df = loadDataFromCSV()
+    st.write(baseDF)
 
 
-def selectTopHeadsets(num):
+def selectTopHeadsets(num, baseDF):
     """
     hashtags filter
     """
@@ -60,9 +65,9 @@ def selectTopHeadsets(num):
 
     # TODO
     # df = loadDataFromDB()
-    df = loadDataFromCSV()
+    # df = loadDataFromCSV()
     
-    top_headsets_list_df = df["Handset Type"].value_counts().nlargest(n=num)
+    top_headsets_list_df = baseDF["Handset Type"].value_counts().nlargest(n=num)
     st.write(top_headsets_list_df)
 
     # Generate plot on top 10 handsets
@@ -74,14 +79,14 @@ def selectTopHeadsets(num):
     st.bar_chart(top_headsets_list_df.sort_values(ascending = False))
 
 
-def topHandsetsByManufacturers(handset, handsetManufac):
+def topHandsetsByManufacturers(handset, handsetManufac, baseDF):
     # TODO
     # df = loadDataFromDB()
-    df = loadDataFromCSV()
+    # df = loadDataFromCSV()
 
-    apple_manufacturer = df.loc[df['Handset Manufacturer'] == 'Apple', ['Handset Type']].value_counts().nlargest(handset)
-    samsung_manufacturer = df.loc[df['Handset Manufacturer'] == 'Samsung', ['Handset Type']].value_counts().nlargest(handset)
-    huawei_manufacturer = df.loc[df['Handset Manufacturer'] == 'Huawei', ['Handset Type']].value_counts().nlargest(handset)
+    apple_manufacturer = baseDF.loc[baseDF['Handset Manufacturer'] == 'Apple', ['Handset Type']].value_counts().nlargest(handset)
+    samsung_manufacturer = baseDF.loc[baseDF['Handset Manufacturer'] == 'Samsung', ['Handset Type']].value_counts().nlargest(handset)
+    huawei_manufacturer = baseDF.loc[baseDF['Handset Manufacturer'] == 'Huawei', ['Handset Type']].value_counts().nlargest(handset)
 
     st.title('Apple')
     st.write(apple_manufacturer)
@@ -93,12 +98,12 @@ def topHandsetsByManufacturers(handset, handsetManufac):
     st.write(huawei_manufacturer)
 
 
-def manufacturerPie(manufacturer_Num):
+def manufacturerPie(manufacturer_Num, baseDF):
     # TODO
     # df = loadDataFromDB()
-    df = loadDataFromCSV()
+    # df = loadDataFromCSV()
 
-    dfLangCount = df['Handset Manufacturer'].value_counts().nlargest(n=manufacturer_Num)
+    dfLangCount = baseDF['Handset Manufacturer'].value_counts().nlargest(n=manufacturer_Num)
     st.title("Top head sets per manufacturer")
     fig1, ax1 = plt.subplots()
     ax1.pie(dfLangCount.values, labels=dfLangCount.keys(), autopct='%.000f%%',
@@ -107,46 +112,46 @@ def manufacturerPie(manufacturer_Num):
     st.pyplot(fig1)
 
 
-def aggregatePerUser():
+def aggregatePerUser(baseDF):
     """
     """
     # TODO
     # df = loadDataFromDB()
-    df = loadDataFromCSV()
+    # df = loadDataFromCSV()
 
     cols_list = ['Bearer Id', 'Dur. (ms)', 'total_data']
     source = st.selectbox("choose aggregate feature", cols_list)
-    df_ = df.groupby('MSISDN/Number')[source].sum()
+    df_ = baseDF.groupby('MSISDN/Number')[source].sum()
     st.title("User aggregate per " + str(source))
     st.write(df_)
     st.bar_chart(data=df_)
 
 st.title('User overview analysis')
 
-displayData()
+displayData(baseDF)
 
 st.markdown("<p style='padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Top handsets</p>", unsafe_allow_html=True)
 
 # Some number in the range 0-100
 num = st.slider('top headsets', 0, 100, 10)
-selectTopHeadsets(num)
+selectTopHeadsets(num, baseDF)
 
 
 st.markdown("<p style='padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Top handsets per top handset manufactures</p>", unsafe_allow_html=True)
 
 handsetNum = st.slider('top headsets', 0, 20, 5)
 man_Num = st.slider('top headset manufacturer', 0, 20, 3)
-topHandsetsByManufacturers(handsetNum, man_Num)
+topHandsetsByManufacturers(handsetNum, man_Num, baseDF)
 
 
 st.markdown("<p style='padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Top handset manufactures</p>", unsafe_allow_html=True)
 
 manufacturer_Num = st.slider('top headset manufacturer', 0, 20, 3, key='manufacturer_Num')
-manufacturerPie(manufacturer_Num)
+manufacturerPie(manufacturer_Num, baseDF)
 
 
 st.markdown("<p style='padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Aggregates per user</p>", unsafe_allow_html=True)
 
-aggregatePerUser()
+aggregatePerUser(baseDF)
 
 
