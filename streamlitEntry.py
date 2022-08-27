b0dda94243a7d87da31ee72e68fbf29d7cb4d466
fish <fisseha.137@gmail.com>
2022-08-27 16:37:06 +0300

    adding helper scripts to path

--- streamlitEntry.py
+++ streamlitEntry.py
@@ -6,3 +6,9 @@ import altair as alt
 from wordcloud import WordCloud
 import plotly.express as px
 
+# import custom libraries
+from data_handler import db_execute_fetch
+from data_handler import DBConnect
+
+# set the page and title
+st.set_page_config(page_title="Twitter data analysis", layout="wide")
