e4222cb8a8f25973013cb0a37313f5a058c663de
fish <fisseha.137@gmail.com>
2022-08-27 16:23:49 +0300

    added the query executor method

--- scripts/data_handler.py
+++ scripts/data_handler.py
@@ -3,7 +3,6 @@ import pandas as pd
 import sqlite3
 from sqlite3 import Error
 
-
 def DBConnect(dbName=None):
     """
     A data base connection creator method.
@@ -21,3 +20,30 @@ def DBConnect(dbName=None):
     conn = sqlite3.connect(dbName)
     print(conn)
     return conn
+
+def execute_query(connection: sqlite3.Connection, query:str) -> None:
+    """
+    A data base query executor method, based on a given connection string and a query string
+
+    Parameters
+    ----------
+    connection :
+        sqlite3.Connection : the database connection
+    query :
+        string : the query string
+    Returns :
+    -------
+    return : nothing
+    """
+
+    cursor = connection.cursor()
+    fd = open(query, 'r')
+    sql_query = fd.read()
+    fd.close()
+
+    try:
+        cursor.execute(sql_query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
