98786d222e7c63d507cdfc0a77790f9c2763facf
fish <fisseha.137@gmail.com>
2022-08-27 16:22:59 +0300

    added the connector method

--- scripts/data_handler.py
+++ scripts/data_handler.py
@@ -3,3 +3,21 @@ import pandas as pd
 import sqlite3
 from sqlite3 import Error
 
+
+def DBConnect(dbName=None):
+    """
+    A data base connection creator method.
+
+    Parameters
+    ----------
+    dbName :
+        Default value = None
+        string : the database name
+
+    Returns :
+        sqlite.connection : the database connection
+    -------
+    """
+    conn = sqlite3.connect(dbName)
+    print(conn)
+    return conn
